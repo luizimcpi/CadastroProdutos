db83e52fe7221edb0e8e48f3138e08c66c6020d3
Luiz Henrique <luizhenrique.se@gmail.com>
2017-12-12 22:18:31 -0200

    Adiciona Classe Model

---
+++ app/src/main/java/cadastroprodutos/devlhse/com/cadastroprodutos/model/Produtos.java
@@ -0,0 +1,47 @@
+package cadastroprodutos.devlhse.com.cadastroprodutos.model;
+
+/**
+ * Created by luizhse on 12/12/17.
+ */
+
+public class Produtos {
+
+    private Long id;
+    private String nomeProduto;
+    private String descricao;
+    private int quantidade;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
