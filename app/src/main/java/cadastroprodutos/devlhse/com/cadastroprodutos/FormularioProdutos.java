11760c908e42334d156ea9b6b878a1b828b859df
Luiz Evangelista <luizhenrique.se@gmail.com>
2017-12-21 10:30:41 -0200

    Add método de inclusão

--- app/src/main/java/cadastroprodutos/devlhse/com/cadastroprodutos/FormularioProdutos.java
+++ app/src/main/java/cadastroprodutos/devlhse/com/cadastroprodutos/FormularioProdutos.java
@@ -1,13 +1,56 @@
 package cadastroprodutos.devlhse.com.cadastroprodutos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import cadastroprodutos.devlhse.com.cadastroprodutos.dao.ProdutosDao;
+import cadastroprodutos.devlhse.com.cadastroprodutos.model.Produto;
 
 public class FormularioProdutos extends AppCompatActivity {
+
+    EditText editText_NomeProd, editText_Descricao, editText_Quantidade;
+    Button btn_Cadastro;
+    Produto editarProduto, produto;
+    ProdutosDao produtosDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_produtos);
+
+        produtosDao = new ProdutosDao(FormularioProdutos.this);
+        Intent intent = getIntent();
+        editarProduto = (Produto) intent.getSerializableExtra("produto-escolhido");
+
+
+        editText_NomeProd = (EditText) findViewById(R.id.editText_NomeProduto);
+        editText_Descricao = (EditText)  findViewById(R.id.editText_Descricao);
+        editText_Quantidade = (EditText) findViewById(R.id.editText_Quantidade);
+
+        btn_Cadastro = (Button) findViewById(R.id.btn_Cadastro);
+
+        if(editarProduto != null){
+            btn_Cadastro.setText("Modificar");
+        }else{
+            btn_Cadastro.setText("Cadastrar");
+        }
+
+        btn_Cadastro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                produto.setNomeProduto(editText_NomeProd.getText().toString());
+                produto.setDescricao(editText_Descricao.getText().toString());
+                produto.setQuantidade(Integer.parseInt(editText_Quantidade.getText().toString()));
+
+                if(btn_Cadastro.getText().toString().equals("Cadastrar")){
+                    produtosDao.salvarProduto(produto);
+                    produtosDao.close();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/cadastroprodutos/devlhse/com/cadastroprodutos/MainActivity.java
+++ app/src/main/java/cadastroprodutos/devlhse/com/cadastroprodutos/MainActivity.java
@@ -1,7 +1,10 @@
 package cadastroprodutos.devlhse.com.cadastroprodutos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCadastrar = (Button) findViewById(R.id.btn_Cadastrar);
+        btnCadastrar.setOnClickListener(new android.view.View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, FormularioProdutos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
